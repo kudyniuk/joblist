bc0db3a955d47f05c2d551ca82ba9951fb0f5cc1
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2024-01-19 09:29:35 +0100

    fix: cors

--- apps/web-server/src/main.ts
+++ apps/web-server/src/main.ts
@@ -9,9 +9,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('/api/v1');
+  app.enableCors()
+
   const config = new DocumentBuilder().setTitle("Joblist API").setDescription("The joblist API description").setVersion("1.0").build()
   const document = SwaggerModule.createDocument(app, config)
-
   SwaggerModule.setup("/api", app, document)
 
   await app.listen(3001)
