75999450b2c8ad587935106e62a258f0c3528833
Wojciech Kudyniuk <79920100+kudyniuk@users.noreply.github.com>
2024-01-19 12:05:58 +0100

    fix: cors (#14)

--- apps/web-server/src/main.ts
+++ apps/web-server/src/main.ts
@@ -7,8 +7,35 @@ import { AppModule } from "./app.module"
 
 config()
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:8000',
+  'http://127.0.0.1:3000',
+  'https://joblist-web-admin.vercel.app'
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    origin: function (origin, callback) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (
+        whitelist.includes(origin) || // Checks your whitelist
+        !!origin.match(/joblist-web-admin\.vercel\.app$/) // Overall check for your domain
+      ) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new ImATeapotException('Not allowed by CORS'), false);
+      }
+    },
+  },)
 
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix("/api/v1")
