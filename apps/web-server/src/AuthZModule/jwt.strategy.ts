597be829106cfddcfd3a4abfca64e9002f00767f
Wojciech Kudyniuk <79920100+kudyniuk@users.noreply.github.com>
2024-01-19 10:52:36 +0100

    fix: cors (#10)

--- apps/web-server/src/AuthZModule/jwt.strategy.ts
+++ apps/web-server/src/AuthZModule/jwt.strategy.ts
@@ -1,10 +1,7 @@
 import { Injectable } from "@nestjs/common"
 import { PassportStrategy } from "@nestjs/passport"
-import { config } from "dotenv"
 import { passportJwtSecret } from "jwks-rsa"
 import { ExtractJwt, Strategy } from "passport-jwt"
-
-config()
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
--- apps/web-server/src/main.ts
+++ apps/web-server/src/main.ts
@@ -1,17 +1,20 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import { config } from "dotenv"
 
 import { AppModule } from "./app.module"
+
+config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
-  app.setGlobalPrefix('/api/v1');
+  app.setGlobalPrefix("/api/v1")
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   })
 
