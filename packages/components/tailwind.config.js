a08a65279fb9d62f077d75a5e9964262e56a4840
Wojciech Kudyniuk <79920100+kudyniuk@users.noreply.github.com>
2024-01-27 18:15:21 +0100

    chore: configs (#22)
    * chore: add parser
    
    * chore: configs

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -2,11 +2,12 @@ import { theme } from "@kudyniuk/theme"
 import { createGlobPatternsForDependencies } from "@nx/react/tailwind"
 import { join } from "path"
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [join(__dirname, "{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}"), ...createGlobPatternsForDependencies(__dirname)],
   theme: {
     extend: theme,
   },
   plugins: [],
 }
+
+export default config
