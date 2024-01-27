d4cb6883726e719077c9d461cbeebccb4889210f
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2024-01-27 19:31:46 +0100

    fix: server

--- apps/web-server/src/app.module.ts
+++ apps/web-server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common"
-import { ServeStaticModule } from "@nestjs/serve-static"
-import { join } from "path"
 
 import { AuthZModule } from "./AuthZModule"
 import { CompaniesModule } from "./CompanyModule"
@@ -9,12 +7,8 @@ import { StatsModule } from "./StatsModule"
 import { UserCompanyModule } from "./UserComapnyModule"
 import { UserJobOffersModule } from "./UserJobOffersModule"
 
-const SwaggerStaticModule = ServeStaticModule.forRoot({
-  rootPath: join(__dirname, "..", "swagger-static"),
-  serveRoot: "/",
-})
 @Module({
-  imports: [AuthZModule, UserCompanyModule, UserJobOffersModule, JobOffersModule, CompaniesModule, StatsModule, SwaggerStaticModule],
+  imports: [AuthZModule, UserCompanyModule, UserJobOffersModule, JobOffersModule, CompaniesModule, StatsModule],
   controllers: [],
   providers: [],
 })
